4a52a0ced0a17da819f28d0e3325ffdc59956cdc
majima0 <ht19a078@oecu.jp>
2021-11-02 12:43:08 +0900

    second

--- src/kadai1.cpp
+++ src/kadai1.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <math.h>
+#include <vector>
+
 #define CPP2_PRIME_UPPER_LIMIT 1000000 //探索する値の上限値。
 /* --------------------------------------------------------------- */
 /*
@@ -12,8 +15,56 @@
 */
 /* -------------------------------------------------------------- */
 int nth_prime(unsigned int a, unsigned int d, unsigned int n);
+
+// 素数判定
+bool IsPrime(int num);
+
 int main() {
-	std::cout << nth_prime(367, 186, 151) << std::endl;
-	// 以下、同様に、入出力例通りになるか確認せよ。
+	int N;  // データセット数
+	std::cin >> N;
+	std::vector<std::vector<unsigned int>> X(N, std::vector<unsigned int>(3));
+	for (int i = 0; i < N; i++) {
+		std::cin >> X[i][0] >> X[i][1] >> X[i][2];
+	}
+
+	for (int i = 0; i < N; i++) {
+		std::cout << nth_prime(X[i][0], X[i][1], X[i][2]) << std::endl;
+	}
+
 	return 0;
+}
+
+int nth_prime(unsigned int a, unsigned int d, unsigned int n) {
+	int ans;  // n番目の素数
+	int ans_num = IsPrime(a);
+	unsigned int cur_num = a;  // n番目の数
+
+	while (CPP2_PRIME_UPPER_LIMIT != ans_num && n != ans_num) {
+		cur_num += d;
+		if (IsPrime(cur_num)) {
+			ans_num++;
+		}
+	}
+	ans = cur_num;
+	return ans;
+}
+
+bool IsPrime(int num)
+{
+	if (num < 2) return false;
+	else if (num == 2) return true;
+	else if (num % 2 == 0) return false; // 偶数はあらかじめ除く
+
+	double sqrtNum = sqrt(num);
+	for (int i = 3; i <= sqrtNum; i += 2)
+	{
+		if (num % i == 0)
+		{
+			// 素数ではない
+			return false;
+		}
+	}
+
+	// 素数である
+	return true;
 }
